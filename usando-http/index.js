3111b401e528eb80c6f20fe93184a12d12b3b27c
Manuel Duarte <carlosd99m@gmail.com>
2021-05-09 14:10:32 -0600

    Envio de respuestas desde el servidor

--- usando-http/index.js
+++ usando-http/index.js
@@ -1,10 +1,15 @@
-const http = require('http')
+const http = require("http");
 const server = http.createServer(onRequest);
+const port = 9999
 
-server.listen(9999)
-console.log("Mi servidor esta corriendo en el localhost:9999")
+server.listen(port, () => {
+  console.log("Mi servidor esta corriendo en el localhost:9999");
+});
 
-function onRequest(request, respose) {
-    console.log("Se ha detectado una nueva petición")
-    
+function onRequest(req, res) {
+  console.log("Se ha detectado una nueva petición");
+
+  res.setHeader("Content-Type", "text/plain");
+  res.write("Welcome of the course node js");
+  res.end();
 }
