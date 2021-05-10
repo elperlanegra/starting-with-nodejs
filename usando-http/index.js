061ab1387a603cddaaa091b4f3778669b912983a
Manuel Duarte <carlosd99m@gmail.com>
2021-05-10 01:34:18 -0600

    Métodos GET, POST, PUT, DELETE

--- usando-http/index.js
+++ usando-http/index.js
@@ -3,6 +3,8 @@ const server = http.createServer(onRequest);
 const port = 9999;
 const fs = require("fs");
 
+const qs = require("querystring");
+
 server.listen(port, () => {
   console.log("Mi servidor esta corriendo en el localhost:9999");
 });
@@ -10,20 +12,56 @@ server.listen(port, () => {
 function onRequest(req, res) {
   console.log("Se ha detectado una nueva petición");
 
-  fs.readFile("index.html", (err, content) => {
-    if (err) {
-      if (err.code == "ENOENT") {
-        res.setStatus = 404;
-        console.log("No se ha encontrado el archivo");
+  if (req.url == "/") {
+    fs.readFile("index.html", (err, content) => {
+      if (err) {
+        if (err.code == "ENOENT") {
+          res.setStatus = 404;
+          console.log("No se ha encontrado el archivo");
+        } else {
+          res.setStatus = 500;
+          console.log("ha ocurrido un error");
+        }
       } else {
-        res.setStatus = 500;
-        console.log("ha ocurrido un error");
+        res.setStatus = 202;
+        res.setHeader("Content-Type", "text/html");
+        res.write(content);
+        res.end();
       }
-    } else {
+    });
+  } else if (req.url === "/users") {
+    if (req.method === "GET") {
       res.setStatus = 202;
       res.setHeader("Content-Type", "text/html");
-      res.write(content);
+      res.write("Accediendo a usuarios");
       res.end();
+    } else if (req.method == "POST") {
+      var datos = "";
+      req.on("data", (d) => {
+        datos += d;
+      });
+      req.on("end", () => {
+        var post = qs.parse(datos);
+        res.end("Datos Recibidos: " + post.nombre);
+      });
+    } else if (req.method == "PUT") {
+      var datos = "";
+      req.on("data", (d) => {
+        datos += d;
+      });
+      req.on("end", () => {
+        var post = qs.parse(datos);
+        res.end("Datos Recibidos: " + post.nombre);
+      });
+    } else if (req.method == "DELETE") {
+      var datos = "";
+      req.on("data", (d) => {
+        datos += d;
+      });
+      req.on("end", () => {
+        var post = qs.parse(datos);
+        res.end("Datos Recibidos: " + post.nombre);
+      });
     }
-  });
+  }
 }
