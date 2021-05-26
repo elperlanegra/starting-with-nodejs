35afaa73d44e36ee039746f7de867b4c588a6e84
Manuel Duarte <carlosd99m@gmail.com>
2021-05-25 23:55:16 -0600

    add coneptos

--- fundamentos-node/conceptos/monohilo.js
+++ fundamentos-node/conceptos/monohilo.js
@@ -1,5 +1,16 @@
 console.log('Hola Mundo')
 
-setInterval(function() {
-    console.log('Sigo activo')
+let i = 0
+
+setInterval(function () {
+    console.log(i)
+    i++
+
+    if (i === 5) {
+        var a = 3 + z
+    }
+
 }, 1000)
+
+
+console.log('Segunda instrucción')
